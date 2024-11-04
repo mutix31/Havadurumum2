3d31e97aa30fddd82397358db9cb0ebf4c7e768d
mutix31 <102666146+mutix31@users.noreply.github.com>
2024-11-04 14:16:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_KEY = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+const API_KEY = 'bd5e378503939ddaee76f12ad7a97608'; // Replace with your OpenWeatherMap API key
 const API_BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 let currentLanguage = 'en';
